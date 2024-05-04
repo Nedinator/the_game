378cfac011c1a0ed03ca1b797531fcde89539165
Nedinator <imnedinator@gmail.com>
2024-05-04 15:58:09 -0500

    some minor changes and adding raycasts to enemy

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+var SPEED = 50
+var direction = 1
+@onready var animated_sprite = $AnimatedSprite2D
+
+@onready var raycast_right = $RaycastRight
+@onready var raycast_left = $RaycastLeft
 
 func _process(delta):
-	position.x += 50 * delta
+	if raycast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	elif raycast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	
+	position.x += direction * SPEED * delta
